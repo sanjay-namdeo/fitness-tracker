b00a2b3f1585e606ce0efa32309ded7ca817b1ba
Sanjay Namdeo <sanjay.namdeo>
2018-11-27 22:36:16 +0100

    Production ready with ng build --prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBGWxcTOCuhER0I4yECd5MH1i8K-9ZVr04',
+    authDomain: 'fitness-tracker-f7d38.firebaseapp.com',
+    databaseURL: 'https://fitness-tracker-f7d38.firebaseio.com',
+    projectId: 'fitness-tracker-f7d38',
+    storageBucket: 'fitness-tracker-f7d38.appspot.com',
+    messagingSenderId: '853099533461'
+  }
 };
